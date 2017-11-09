a8246e7957325ee6542ea95d4c7cd4bdd98e2992
Oriol Riu Gispert <oriolrg@gmail.com>
2017-11-09 17:38:30 +0100

    borron i cuenta nueva

--- src/app/app.constants.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Configuration {
-    public Server = 'http://localhost/';
-    public ApiUrl = 'restaurant/';
-    public ServerWithApiUrl = this.Server + this.ApiUrl;
-}
