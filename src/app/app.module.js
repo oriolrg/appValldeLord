3460c4aa079f6e0d8d2e525d0eb72080e79abf31
Oriol Riu Gispert <oriolrg@gmail.com>
2017-11-22 18:51:18 +0100

    app definitiva ja entregada i compilada funcional

---
+++ src/app/app.module.js
@@ -0,0 +1,60 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpModule } from '@angular/http';
+import { IonicImageLoader } from 'ionic-image-loader';
+import { Network } from '@ionic-native/network';
+//import { ReversePipe } from 'ngx-pipes/src/app/pipes/array/reverse';
+import { RestaurantPage } from '../pages/restaurant/restaurant';
+import { InformacioPage } from '../pages/informacio/informacio';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { InfoRestaurantPage } from '../pages/info-restaurant/info-restaurant';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { DatabaseProvider } from '../providers/database/database';
+var AppModule = (function () {
+    function AppModule() {
+    }
+    AppModule.decorators = [
+        { type: NgModule, args: [{
+                    declarations: [
+                        MyApp,
+                        RestaurantPage,
+                        InformacioPage,
+                        HomePage,
+                        TabsPage,
+                        InfoRestaurantPage
+                    ],
+                    imports: [
+                        BrowserModule,
+                        IonicModule.forRoot(MyApp),
+                        HttpModule,
+                        IonicImageLoader.forRoot()
+                    ],
+                    bootstrap: [IonicApp],
+                    entryComponents: [
+                        MyApp,
+                        RestaurantPage,
+                        InformacioPage,
+                        HomePage,
+                        TabsPage,
+                        InfoRestaurantPage
+                    ],
+                    providers: [
+                        StatusBar,
+                        SplashScreen,
+                        { provide: ErrorHandler, useClass: IonicErrorHandler },
+                        DatabaseProvider,
+                        HttpModule,
+                        Network
+                    ]
+                },] },
+    ];
+    /** @nocollapse */
+    AppModule.ctorParameters = function () { return []; };
+    return AppModule;
+}());
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
